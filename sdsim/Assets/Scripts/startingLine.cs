4c98b3f5b7b2f066d36f9dee2d7ea18e8d2829b5
Adam McAleer <amcaleer1127@gmail.com>
2023-03-16 20:19:15 -0400

    first commit I thinks

---
+++ sdsim/Assets/Scripts/startingLine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startingLine : MonoBehaviour
+{
+    public int index = 0;
+    string target = "body";
+    PrivateAPI privateAPI;
+
+    void Start()
+    {
+        privateAPI = GameObject.FindObjectOfType<PrivateAPI>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name != target) { return; }
+        float time = Time.fixedTime;
+        
+
+        Transform parent = col.transform.parent.parent;
+        if (parent == null) { return; }
+
+        string carName = parent.name;
+        tk.TcpCarHandler client = parent.GetComponentInChildren<tk.TcpCarHandler>();
+
+        if (client != null)
+            UnityMainThreadDispatcher.Instance().Enqueue(client.SendCollisionWithStartingLine(index, time));
+
+        if (privateAPI != null)
+            privateAPI.CollisionWithStatingLine(carName, index, time);
+    }
+}
