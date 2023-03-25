3abb66b30d094e605a747977acb733c9cad3d043
Adam McAleer <amcaleer1127@gmail.com>
2023-03-24 20:08:51 -0400

    Added additional collision info

--- sdsim/Assets/AutoCol.cs
+++ sdsim/Assets/AutoCol.cs
@@ -7,18 +7,21 @@ public class AutoCol : MonoBehaviour
 
     private int hits;
     private int lapNum;
+    private int hitsCurrentLap;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag.Equals("Finish") == true)
         {
             lapNum = lapNum + 1;
-            Debug.Log("Lap: " + lapNum);
+            hitsCurrentLap = 0;
+            Debug.Log("Lap: " + lapNum + " |Total hits: " + hits);
         }
         else
         {
             hits = hits + 1;
-            Debug.Log("Hit! Count: " + hits);
+            hitsCurrentLap = hitsCurrentLap + 1;
+            Debug.Log("Lap: " + lapNum + " |Hits this lap: " + hitsCurrentLap + " |Total hits: " + hits);
         }
         
     }
