13d9b39005a524a0eac4556866c62c28c098b15b
Adam McAleer <amcaleer1127@gmail.com>
2023-04-15 20:15:12 -0400

    Added autoCol to mountain track

---
+++ sdsim/Assets/track_col.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class track_col : MonoBehaviour
+{
+    void onTriggerLeave(Collider other)
+    {
+        Debug.Log("Left track");
+    }
+}
