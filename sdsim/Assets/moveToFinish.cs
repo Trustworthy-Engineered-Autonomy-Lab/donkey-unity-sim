8ce80595ab6cbdcf84869b3df5793f36361bffe2
Adam McAleer <amcaleer1127@gmail.com>
2023-04-15 20:04:52 -0400

    Added reset coordinates for mountain track

--- sdsim/Assets/moveToFinish.cs
+++ sdsim/Assets/moveToFinish.cs
@@ -8,8 +8,14 @@ public class moveToFinish : MonoBehaviour
     {
         if (isColliding == 1)
         {
-            transform.position = new Vector3(13.03789f, 0.5591203f, -68.18274f);
-            transform.rotation = Quaternion.Euler(-0.007f, -136.209f, 0.005f);
+            //for mini monaco
+            //transform.position = new Vector3(13.03789f, 0.5591203f, -68.18274f);
+            //transform.rotation = Quaternion.Euler(-0.007f, -136.209f, 0.005f);
+
+            //for mountain track
+            transform.position = new Vector3(50.07488f, 5.234812f, 51.09399f);
+            transform.rotation = Quaternion.Euler(1.625f, 178.326f, 0.116f);
+
         }
     }
 }
