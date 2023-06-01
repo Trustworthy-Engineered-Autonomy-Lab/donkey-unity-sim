bd7c3d172035472ff6248375e701c09a5314e820
Adam McAleer <amcaleer1127@gmail.com>
2023-05-31 20:25:24 -0400

    Fixed distance to track origin

--- sdsim/Assets/centerLine_Distance.cs
+++ sdsim/Assets/centerLine_Distance.cs
@@ -8,23 +8,19 @@ public class centerLine_Distance : MonoBehaviour
 
     public float dist = Mathf.Infinity;
     public GameObject obj;
-    public int t;
-    public VertexPath vertexPath;
+
     // Start is called before the first frame update
     void Start()
     {
-        vertexPath = new VertexPath();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        t = 1;
 
-        if (vertexPath != null)
-        {
-            dist = Vector3.Distance(transform.position, vertexPath.GetPoint(t));
-            Debug.DrawLine(transform.position, vertexPath.GetPoint(t), Color.green);
-        }
+        dist = Vector3.Distance(transform.position, obj.transform.position);
+        Debug.DrawLine(transform.position, obj.transform.position, Color.green);
+        
     }
 }
