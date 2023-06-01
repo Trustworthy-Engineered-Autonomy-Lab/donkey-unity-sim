2120ddcbdfd4c25e1853a58d131a3c5bc8a63be4
Adam McAleer <amcaleer1127@gmail.com>
2023-05-31 21:34:19 -0400

    Able to find the distance from track center to car, show with red line

--- sdsim/Assets/centerLine_Distance.cs
+++ sdsim/Assets/centerLine_Distance.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PathCreation; 
 
-public class centerLine_Distance : MonoBehaviour
+namespace PathCreation.Examples
 {
-
-    public float dist = Mathf.Infinity;
-    public GameObject obj;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class centerLine_Distance : MonoBehaviour
     {
 
-        dist = Vector3.Distance(transform.position, obj.transform.position);
-        Debug.DrawLine(transform.position, obj.transform.position, Color.green);
-        
+        public PathCreator pathCreator;
+        public float dist = 0;
+        float distanceTravelled;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (pathCreator != null)
+            {
+                // Subscribed to the pathUpdated event so that we're notified if the path changes during the game
+                pathCreator.pathUpdated += OnPathChanged;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (pathCreator != null)
+            {
+
+                Vector3 closestPoint = pathCreator.path.GetClosestPointOnPath(transform.position);
+                dist = Vector3.Distance(transform.position, closestPoint);
+                Debug.DrawLine(transform.position, closestPoint, Color.red);
+            }
+        }
+
+        void OnPathChanged()
+        {
+            distanceTravelled = pathCreator.path.GetClosestDistanceAlongPath(transform.position);
+        }
     }
 }
